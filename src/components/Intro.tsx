587521583aefbc662ca39ed90ee52f4db8290f4b
Ryan Biondo <biondoryan.dev@gmail.com>
2023-07-14 22:04:58 -0600

    Improved responsiveness of the website. Added a new project to the portfolio. Added a new screenshot for the portfolio.

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -9,7 +9,9 @@ const Intro = () => {
     <>
       <Socials />
       <section className={styles.introSection} id="intro">
-        <h1>Ryan Biondo</h1>
+        <h1 className={styles.slidingDownDiv}>
+          Ryan <span className={styles.biondoColor}>Biondo</span>
+        </h1>
         <p className={styles.jobTitle}>Front End Developer</p>
         <p className={styles.downloadResume}>Download my resume here!</p>
         <a href={resume} download>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,25 +5,29 @@ import profilepic from '/src/images/profilepic.jpg';
 
 const NavBar = () => {
   const [navOpen, setNavOpen] = useState(false);
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth > 768) {
-        setNavOpen(false);
-      }
-    };
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
+  const handleResize = () => {
+    setIsMobile(window.innerWidth <= 768);
+    if (window.innerWidth > 768) {
+      setNavOpen(false);
+    }
+  };
+
+  useEffect(() => {
     window.addEventListener('resize', handleResize);
 
     return () => {
       window.removeEventListener('resize', handleResize);
     };
   }, []);
+
   const toggleNav = () => {
     setNavOpen(!navOpen);
   };
 
   return (
-    <nav>
+    <nav className={styles.navbar}>
       <div className={styles.navTitle}>
         <a href="#intro">
           <img className={styles.navPic} src={profilepic} alt="Ryan" />
@@ -33,13 +37,20 @@ const NavBar = () => {
         </a>
       </div>
 
-      {navOpen ? (
-        <BiX className={styles.menuIcon} onClick={toggleNav} />
-      ) : (
-        <BiMenu className={styles.menuIcon} onClick={toggleNav} />
-      )}
+      {isMobile &&
+        (navOpen ? (
+          <BiX
+            className={`${styles.menuIcon} ${styles.open}`}
+            onClick={toggleNav}
+          />
+        ) : (
+          <BiMenu className={styles.menuIcon} onClick={toggleNav} />
+        ))}
 
-      <ul className={navOpen ? styles.navLinksActive : styles.navLinks}>
+      <ul
+        className={
+          navOpen ? `${styles.navLinksActive} ${styles.open}` : styles.navLinks
+        }>
         <a href="#intro">
           <li className={styles.navItem}>Home</li>
         </a>
